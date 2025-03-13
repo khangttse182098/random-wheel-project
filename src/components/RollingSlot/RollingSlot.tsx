a935d31b3d0ee25fce33571034bacb43c58123c4
khangttse182098@fpt.edu.vn <khangttse182098@fpt.edu.vn>
2025-03-13 17:56:05 +0700

    fix scss and RollingSlot

--- src/components/RollingSlot/RollingSlot.tsx
+++ src/components/RollingSlot/RollingSlot.tsx
@@ -1,130 +1,91 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useRef } from "react";
 import gsap from "gsap";
+import useAppStore from "../../store/useAppStore";
+import style from "../../pages/SpinPage/SpinPage.module.scss";
 
 interface RollingSlotProps {
-  value: number;
+  value: string;
   delay: number;
 }
 
-const getRandomEvenHundred = () => {
-  const min = -7200;
-  const max = 0;
-  const step = 200; // The step size (even hundred)
+const charList = [
+  "0",
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
 
-  // Generate a random integer between 0 and (max-min)/step
-  const randomMultiplier = Math.floor(Math.random() * ((max - min) / step + 1));
-
-  // Calculate the final number
-  return min + randomMultiplier * step;
+const getAnimationPosition = (position: number) => {
+  return position * -200;
 };
 
 const RollingSlot: React.FC<RollingSlotProps> = ({ value, delay }) => {
   const slotRef = useRef<HTMLUListElement>(null);
-  // const [displayValue, setDisplayValue] = useState(0);
-
+  const { setIsSlotSpinning } = useAppStore((state) => state);
   useEffect(() => {
-    // let interval: NodeJS.Timeout;
-    // if (slotRef.current) {
-    const randomY = getRandomEvenHundred();
-    gsap.fromTo(
-      slotRef.current,
-      { y: -14200, opacity: 1 }, // Bắt đầu dịch chuyển lên trên (-100px) và ẩn đi
-      {
-        y: randomY, // Trả về vị trí gốc
-        opacity: 1, // Hiển thị lại
-        duration: 3, // animation khoảng 1.5s
-        ease: "power2.inOut", // Hiệu ứng chuyển động
-        delay: delay,
-        // repeat: -1,
-        // onStart: () => {
-        //   interval = setInterval(() => {
-        //     setDisplayValue((prev) => (prev + 1) % 10);
-        //   }, 100);
-        // },
-        // onComplete: () => {
-        //   clearInterval(interval);
-        //   setDisplayValue(value);
-        // },
-      }
-    );
-    // }
-  }, [value, delay]);
+    const position = charList.indexOf(value);
+    const randomY = getAnimationPosition(position);
+    if (position != -1) {
+      gsap.fromTo(
+        slotRef.current,
+        { y: -14200 }, // Bắt đầu dịch chuyển lên trên (-100px) và ẩn đi
+        {
+          y: randomY, // Trả về vị trí gốc
+          duration: 3, // animation khoảng 1.5s
+          ease: "power3.inOut", // Hiệu ứng chuyển động
+          delay: delay,
+          onStart: () => {
+            setIsSlotSpinning(true);
+          },
+          onComplete: () => {
+            setIsSlotSpinning(false);
+          },
+        }
+      );
+    }
+  }, [value, delay, setIsSlotSpinning]);
 
   return (
-    <div className="slot">
-      <ul ref={slotRef} className="slot-number">
-        <li>0</li>
-        <li>1</li>
-        <li>2</li>
-        <li>3</li>
-        <li>4</li>
-        <li>5</li>
-        <li>6</li>
-        <li>7</li>
-        <li>8</li>
-        <li>9</li>
-        <li>A</li>
-        <li>B</li>
-        <li>C</li>
-        <li>D</li>
-        <li>E</li>
-        <li>F</li>
-        <li>G</li>
-        <li>H</li>
-        <li>I</li>
-        <li>J</li>
-        <li>K</li>
-        <li>L</li>
-        <li>M</li>
-        <li>N</li>
-        <li>O</li>
-        <li>P</li>
-        <li>Q</li>
-        <li>R</li>
-        <li>S</li>
-        <li>T</li>
-        <li>U</li>
-        <li>V</li>
-        <li>W</li>
-        <li>X</li>
-        <li>Y</li>
-        <li>Z</li>
-        <li>0</li>
-        <li>1</li>
-        <li>2</li>
-        <li>3</li>
-        <li>4</li>
-        <li>5</li>
-        <li>6</li>
-        <li>7</li>
-        <li>8</li>
-        <li>9</li>
-        <li>A</li>
-        <li>B</li>
-        <li>C</li>
-        <li>D</li>
-        <li>E</li>
-        <li>F</li>
-        <li>G</li>
-        <li>H</li>
-        <li>I</li>
-        <li>J</li>
-        <li>K</li>
-        <li>L</li>
-        <li>M</li>
-        <li>N</li>
-        <li>O</li>
-        <li>P</li>
-        <li>Q</li>
-        <li>R</li>
-        <li>S</li>
-        <li>T</li>
-        <li>U</li>
-        <li>V</li>
-        <li>W</li>
-        <li>X</li>
-        <li>Y</li>
-        <li>0</li>
+    <div className={style["slot"]}>
+      <ul ref={slotRef} className={style["slot-number"]}>
+        {charList.map((char) => (
+          <li>{char}</li>
+        ))}
+        {charList.map((char) => (
+          <li>{char}</li>
+        ))}
       </ul>
     </div>
   );
--- src/pages/SpinPage/SpinPage.tsx
+++ src/pages/SpinPage/SpinPage.tsx
@@ -1,32 +1,35 @@
 import { useState } from "react";
 import RollingSlot from "../../components/RollingSlot/RollingSlot";
-import "./SpinPage.scss";
+import style from "./SpinPage.module.scss";
+import useAppStore from "../../store/useAppStore";
 
-const SpinPage = () => {
-  const [numSlots, setNumSlots] = useState(5);
-  const [numbers, setNumbers] = useState<number[]>(Array(numSlots).fill(0));
-  const [spinKey, setSpinKey] = useState(0); // Add a key to force re-mount
+const mockData = ["AI4SZ", "01234", "LMFAO"];
+
+//NHỚ SỬA MOCKDATA THÀNH VALUE KHI SỬA LẠI BÊN TRANG EVENT-SETTING
+const SpinPage = ({ value }: { value: string[] }) => {
+  const [code, setCode] = useState<string[]>(
+    Array(mockData[0].length).fill("")
+  );
+  const [spinKey, setSpinKey] = useState(0);
+  const { isSlotSpinning } = useAppStore((state) => state);
+
+  const handldeGetRandomCode = () => {
+    // const selectedCode = value[Math.floor(Math.random() * value.length)];
+    const selectedCode = mockData[Math.floor(Math.random() * mockData.length)];
+    return selectedCode.split("");
+  };
 
   const spin = () => {
-    const newNumbers = Array.from({ length: numSlots }, () =>
-      Math.floor(Math.random() * 10)
-    );
-    setNumbers(newNumbers);
+    const randomCode = handldeGetRandomCode();
+    setCode(randomCode);
     setSpinKey((prevKey) => prevKey + 1); // Update the key to force re-mount
   };
 
   return (
-    <div className="container">
+    <div className={style["container"]}>
       <h2>Máy Quay Số</h2>
-      <input
-        type="number"
-        value={numSlots}
-        onChange={(e) => setNumSlots(Number(e.target.value))}
-        min={1}
-        max={10}
-      />
-      <div className="slot-machine">
-        {numbers.map((num, index) => (
+      <div className={style["slot-machine"]}>
+        {code.map((num, index) => (
           <RollingSlot
             key={`${spinKey}-${index}`}
             value={num}
@@ -34,7 +37,9 @@ const SpinPage = () => {
           />
         ))}
       </div>
-      <button onClick={spin}>Quay</button>
+      <button onClick={spin} disabled={isSlotSpinning}>
+        Quay
+      </button>
     </div>
   );
 };
