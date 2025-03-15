00f811bc290e7dd71c2c5b70a72b5d496014eaa3
khangttse182098@fpt.edu.vn <khangttse182098@fpt.edu.vn>
2025-03-15 17:00:33 +0700

    add eventSetting data from zustand to SpinPage component

--- src/layout/SpinLayout/SpinLayout.tsx
+++ src/layout/SpinLayout/SpinLayout.tsx
@@ -1,9 +1,20 @@
 import SpinPage from "../../pages/SpinPage/SpinPage";
-import "./SpinLayout.scss";
+import useAppStore from "../../store/useAppStore";
+import style from "./SpinLayout.module.scss";
 
 const SpinLayout = () => {
+  const { eventSetting } = useAppStore.getState();
+  console.log(eventSetting);
+
   return (
-    <div className="page-container">
+    <div
+      className={style["page-container"]}
+      style={
+        {
+          "--bg": `url(${eventSetting?.backgroundImage})`,
+        } as React.CSSProperties
+      }
+    >
       <SpinPage />
     </div>
   );
