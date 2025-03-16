faf2cc580f2a582d9c2f6a74cb5a33321842a46a
minhle <minhldse181522@fpt.edu.vn>
2025-03-17 00:10:47 +0700

    add result for spin and set rolling turns followed by reward

--- src/service/event/api.ts
+++ src/service/event/api.ts
@@ -2,6 +2,7 @@ import api from "../../config/axios";
 import { CreateEventData, UpdateEventData } from "../../models/event";
 import { UpdateEventSettingData } from "../../models/eventSetting";
 import { CreateRewardData } from "../../models/reward";
+import { SaveWinnerData } from "../../models/winner";
 
 // Danh sách sự kiện
 export const getEventList = () => {
@@ -44,4 +45,14 @@ export const getWinnerList = (eventId: string) => {
 // Tạo giải thưởng
 export const createAward = (data: CreateRewardData) => {
   return api.post("/api/v1/rewards", data);
+};
+
+//Lấy số lượt quay dựa trên giải
+export const getRollingNumber = (rewardId: number) => {
+  return api.get(`/api/v1/rewards/${rewardId}`);
+};
+
+// Lưu người trúng thưởng
+export const saveWinner = (data: SaveWinnerData) => {
+  return api.post("/api/v1/reward-winner/event/winner-list", data);
 };
